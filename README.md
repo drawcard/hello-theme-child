4c623680c80a93dc5f5edbab5b94fbf0653c5679
Drawcard <git@drawcard.com.au>
2020-01-27 14:04:24 +1030

    master: Added webpack

---
+++ README.md
@@ -0,0 +1,12 @@
+# Hello Theme Child
+
+by Drawcard
+
+## Getting Started
+
+- `npm i` installs dependencies
+
+## Javascript
+
+- Add your imports and custom JS code to `src\hello-theme-child.src.js`
+- `npm run build` runs Webpack and builds `dist\hello-theme-child.min.js`
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+// Webpack Config File
+const path = require('path');
+
+module.exports = {
+  // Path to the source file
+  entry: './src/hello-theme-child.src.js',
+  output: {
+    // Output to the destination file
+    filename: 'hello-theme-child.min.js',
+    // Set path directory to /dist/
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
